a97f7c1cb6546be37f8640e6d28d9a05298a1e56
Charlie Merrell <charlie.merrell20@gmail.com>
2021-06-19 12:58:50 +0100

    adds non-functional sign up page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
-import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import blue from "@material-ui/core/colors/blue";
+import SignUp from "./SignUp";
+
+const darkTheme = createMuiTheme({
+    palette: {
+        type: "dark",
+        primary: blue,
+    },
+});
 
 function App() {
     fetch("/api/hello").then(console.log);
     return (
-        <div className="App">
-            <Button variant="contained" color="primary">
-                Hello World
-            </Button>
-        </div>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <SignUp />;
+        </ThemeProvider>
     );
 }
 
