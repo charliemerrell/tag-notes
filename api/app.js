cc36e1a3301adae693356c253227a027d4e26a3a
Charlie Merrell <charlie.merrell20@gmail.com>
2021-06-19 07:44:11 +0100

    readies for deployment

--- api/app.js
+++ api/app.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, "../build")));
+
+app.get("/api/hello", (req, res) => {
+    res.send("Hello, World");
+});
 
 module.exports = app;
--- api/bin/server.js
+++ api/bin/server.js
@@ -1,7 +1,3 @@
 const app = require("../app");
 
-app.get("/api/hello", (req, res) => {
-    res.send("Hello, World");
-});
-
 app.listen(4000);
